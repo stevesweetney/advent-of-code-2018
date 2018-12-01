468d390f49d58c95824e1780c46d51a5e37ec9e4
Steve Sweetney <stevesweetney@gmail.com>
2018-12-01 11:55:54 -0500

    Initial commit

---
+++ src/day1.rs
@@ -0,0 +1,25 @@
+use std::i32;
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &str) -> i32 {
+    input.lines().map(|l| l.parse::<i32>().unwrap()).sum()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::solve_part1;
+
+    #[test]
+    fn should_return_3() {
+        let input = "0\n-2\n5";
+        assert_eq!(3, solve_part1(input));
+    }
+
+    #[test]
+    fn should_return_55() {
+        let data = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+        let input = data.join("\n");
+        assert_eq!((10 * 11) / 2, solve_part1(&input));
+    }
+}
