dedcfae451044c372984fa77072b5bfe8f31d0b1
Steve Sweetney <stevesweetney@gmail.com>
2018-12-08 19:58:34 -0500

    Day 6 part 1

---
+++ src/day6.rs
@@ -0,0 +1,169 @@
+use kdtree::KdTree;
+use std::{
+    cmp,
+    collections::{HashMap, HashSet},
+    f32,
+    num::ParseIntError,
+    str::FromStr,
+    u32::MAX,
+};
+
+fn manhattan_distance(p1: &[f32], p2: &[f32]) -> f32 {
+    p1.iter()
+        .zip(p2)
+        .fold(0.0, |acc, (a, b)| acc + f32::abs(a - b))
+}
+
+#[aoc_generator(day6)]
+fn input_gen(input: &str) -> Vec<Point> {
+    input.lines().map(|l| l.parse::<Point>().unwrap()).collect()
+}
+
+#[aoc(day6, part1)]
+fn solve_part1(input: &[Point]) -> u32 {
+    let (rect, points_tree) = process_tree_and_rect(input);
+    let mut infinite_areas = HashSet::<usize>::new();
+
+    for p in rect.perimeter_list() {
+        let nearest = points_tree
+            .nearest(&[p.x as f32, p.y as f32], 2, &manhattan_distance)
+            .unwrap();
+        if nearest[0].0 != nearest[1].0 {
+            infinite_areas.insert(*nearest[0].1);
+        }
+    }
+
+    let mut area_map = HashMap::new();
+    let mut best = 0;
+    for p in rect.points_iter() {
+        let nearest = points_tree
+            .nearest(&[p.0 as f32, p.1 as f32], 2, &manhattan_distance)
+            .unwrap();
+        if nearest[0].0 != nearest[1].0 && !infinite_areas.contains(nearest[0].1) {
+            let area = area_map.entry(nearest[0].1).or_insert(0);
+            *area += 1;
+            //println!("{} -- {:?}", area, nearest[0].1);
+            best = cmp::max(best, *area);
+        }
+    }
+    best
+}
+
+fn process_tree_and_rect(points: &[Point]) -> (Rectangle, PointsTree) {
+    let (mut min_x, mut max_x, mut min_y, mut max_y) = (MAX, 0, MAX, 0);
+    let mut tree = KdTree::new_with_capacity(2, points.len());
+
+    for (i, p) in points.iter().enumerate() {
+        min_x = cmp::min(min_x, p.x);
+        min_y = cmp::min(min_y, p.y);
+        max_x = cmp::max(max_x, p.x);
+        max_y = cmp::max(max_y, p.y);
+        tree.add([p.x as f32, p.y as f32], i);
+    }
+
+    (
+        Rectangle {
+            upper_left: Point::new(min_x, min_y),
+            upper_right: Point::new(max_y, min_y),
+            lower_left: Point::new(min_x, max_y),
+            lower_right: Point::new(max_x, max_y),
+        },
+        tree,
+    )
+}
+
+#[derive(Debug)]
+pub struct Point {
+    x: u32,
+    y: u32,
+}
+
+impl Point {
+    fn new(x: u32, y: u32) -> Self {
+        Point { x, y }
+    }
+}
+
+impl FromStr for Point {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let coords: Vec<&str> = s.split(",").map(|s| s.trim()).collect();
+        Ok(Point {
+            x: coords[0].parse::<u32>()?,
+            y: coords[1].parse::<u32>()?,
+        })
+    }
+}
+
+type PointsTree = KdTree<f32, usize, [f32; 2]>;
+
+struct Rectangle {
+    upper_left: Point,
+    upper_right: Point,
+    lower_left: Point,
+    lower_right: Point,
+}
+
+impl Rectangle {
+    fn perimeter_list(&self) -> Vec<Point> {
+        let mut points = Vec::new();
+        for x in self.upper_left.x..=self.upper_right.x {
+            points.push(Point::new(x, self.upper_left.y));
+            points.push(Point::new(x, self.lower_left.y));
+        }
+
+        for y in self.upper_left.y..=self.lower_left.y {
+            points.push(Point::new(self.upper_left.x, y));
+            points.push(Point::new(self.upper_right.x, y));
+        }
+
+        points
+    }
+
+    fn points_iter(&self) -> PointsIter {
+        PointsIter {
+            rect: self,
+            px: self.upper_left.x,
+            py: self.upper_left.y,
+        }
+    }
+}
+
+struct PointsIter<'r> {
+    rect: &'r Rectangle,
+    px: u32,
+    py: u32,
+}
+
+impl<'r> Iterator for PointsIter<'r> {
+    type Item = (u32, u32);
+    fn next(&mut self) -> Option<(u32, u32)> {
+        if self.px > self.rect.upper_right.x {
+            self.px = self.rect.upper_left.x;
+            self.py += 1;
+        }
+        if self.py > self.rect.lower_right.y {
+            return None;
+        }
+        let (x, y) = (self.px, self.py);
+        self.px += 1;
+        Some((x, y))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = input_gen(include_str!("../input/tests/d6.txt"));
+        assert_eq!(solve_part1(&input), 17);
+    }
+
+    #[test]
+    fn manhattan_distance_test() {
+        assert_eq!(manhattan_distance(&[0.0, 0.0], &[3.0, 10.0]), 13.0);
+    }
+}
